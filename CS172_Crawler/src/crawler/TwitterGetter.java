ebf8eaf157cdc6e30eed46d8eb587ac7fc05afd4
Jeffrey C <jeff2435@gmail.com>
2015-10-28 03:31:57 -0700

    WebGetter: Now works!, TwitterGetter: Not working - Need to customize Gson JSON parse for geolocation location search return

--- CS172_Crawler/src/crawler/TwitterGetter.java
+++ CS172_Crawler/src/crawler/TwitterGetter.java
@@ -7,19 +7,31 @@ import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.xml.crypto.Data;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 import com.google.gson.Gson;
+public class Id{
+	private String identifier;
+	private String url;
+	private String place_type;
+	private String name;
+	private String country;
+	
+	@Override
+	public String toString(){
+		return name + " " + country;
+	}
+}
+
+public class Places{
+	private Id ID;
+}
 
 public class TwitterGetter {
 
 	public static void main(String[] args) throws MalformedURLException, IOException {
 		String consumer_key = "&oauth_consumer_key=k2Ykj3xZkolzy0NYFGRAZqbTg";
 		String access_token = "&oauth_token=1499500044-FvdxAP0owsCyAmVGZ4dbrSHUOQN5te8l0tX3Uld";
-		String signature_method = "&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1446024385&oauth_nonce=3Vp41N&oauth_version=1.0&oauth_signature=4VkOspk9JboXDJy4DgP9xGjhnlQ%3D";
+		String signature_method = "&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1446024385&"
+				+ "oauth_nonce=3Vp41N&oauth_version=1.0&oauth_signature=4VkOspk9JboXDJy4DgP9xGjhnlQ%3D";
 		
 		String query = "Toronto";
 		
@@ -29,7 +41,9 @@ public class TwitterGetter {
 		
 		InputStream input = new URL(url).openStream();
 		Reader reader = new InputStreamReader(input, "UTF-8");
-		Data data = new Gson().fromJson(reader, Data.class);
+		Places data = new Gson().fromJson(reader, Places.class);
+		System.out.println(data);
+		
 	}
 
 }
