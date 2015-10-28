33c150f8af9aa3632f7bd78d4339eb4ed76cd44f
Jeffrey C <jeff2435@gmail.com>
2015-10-28 03:06:47 -0700

    break apart crawler into TwitterGetter and WebGetter

---
+++ CS172_Crawler/src/crawler/TwitterGetter.java
@@ -0,0 +1,35 @@
+package crawler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.crypto.Data;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import com.google.gson.Gson;
+
+public class TwitterGetter {
+
+	public static void main(String[] args) throws MalformedURLException, IOException {
+		String consumer_key = "&oauth_consumer_key=k2Ykj3xZkolzy0NYFGRAZqbTg";
+		String access_token = "&oauth_token=1499500044-FvdxAP0owsCyAmVGZ4dbrSHUOQN5te8l0tX3Uld";
+		String signature_method = "&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1446024385&oauth_nonce=3Vp41N&oauth_version=1.0&oauth_signature=4VkOspk9JboXDJy4DgP9xGjhnlQ%3D";
+		
+		String query = "Toronto";
+		
+		String api_base = "https://api.twitter.com/1.1/geo/search.json?";
+		
+		String url = api_base+"query="+query+consumer_key+access_token+signature_method;
+		
+		InputStream input = new URL(url).openStream();
+		Reader reader = new InputStreamReader(input, "UTF-8");
+		Data data = new Gson().fromJson(reader, Data.class);
+	}
+
+}
--- CS172_Crawler/src/crawler/WebGetter.java
+++ CS172_Crawler/src/crawler/WebGetter.java
@@ -5,27 +5,21 @@ import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 
-import org.json.*;
-
 public class WebGetter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String html = "<html><head><title>First parse</title></head>"
-				+ "<body><p>Parsed HTML into a doc.</p></body></html>";
-		Document doc2 = Jsoup.parse(html);
-		System.out.println(doc2.title());
-		
-		String html2 = "http://www.ucr.edu/";
+		//THIS CODE WORKS FOR DOWNLOADING WEBPAGES
+		String html = "http://www.ucr.edu/";
 		Document doc = null;
 		try {
-			doc = Jsoup.connect(html2).get();
+			doc = Jsoup.connect(html).get();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println(doc.title());
-		
+		System.out.println(doc.head());
+		System.out.println(doc.body());
 	}
 
 }
