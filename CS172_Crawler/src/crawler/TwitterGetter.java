c0d5a3d865763c37ac1b0e4baf75da38f4a21883
Spencer Lee <slee163@ucr.edu>
2015-10-31 01:47:55 -0700

    Fixed java class errors, .gitignore, and files.
    Fixed java class error by creating new class files.
    Updated .gitignore to now ignore local library and project references
    Removed unecessary package.

--- CS172_Crawler/src/crawler/TwitterGetter.java
+++ CS172_Crawler/src/crawler/TwitterGetter.java
@@ -7,23 +7,8 @@ import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import com.google.gson.Gson;
-public class Id{
-	private String identifier;
-	private String url;
-	private String place_type;
-	private String name;
-	private String country;
-	
-	@Override
-	public String toString(){
-		return name + " " + country;
-	}
-}
 
-public class Places{
-	private Id ID;
-}
+import com.google.gson.Gson;
 
 public class TwitterGetter {
 
