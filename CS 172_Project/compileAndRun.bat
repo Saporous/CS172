e5bf156ad959f26a7946aa8cae10f02e5e7976a7
Jeffrey C <jeff2435@gmail.com>
2015-11-03 11:19:53 -0800

    Documentation stuff
    Renamed .bat file, report almost done, architecture diagram still in progress

--- CS 172_Project/compileAndRun.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set PATH=%PATH%;C:\Program Files\Java\jdk1.8.0_65\bin
-
-javac -cp "lib/*" -d . ./src/crawler/*.java
-
-jar cf TwitterGetter.jar ./crawler/*.class
-
-java -cp "lib/*;TwitterGetter.jar" crawler.TwitterGetter %1 %2
