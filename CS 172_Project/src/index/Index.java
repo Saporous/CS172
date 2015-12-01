6ba51b93261ab62c84e174ae9f6601b5f88da62a
Spencer Lee <slee163@ucr.edu>
2015-11-30 20:37:43 -0800

    Successfully parses and appears that index may work
    However index files are not produced

--- CS 172_Project/src/index/Index.java
+++ CS 172_Project/src/index/Index.java
@@ -1,44 +1,30 @@
 package index;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.ParseException;
-import java.util.List;
-import java.util.Set;
+
 
 import org.apache.lucene.*;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.DateTools;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.DoubleField;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.FloatField;
-import org.apache.lucene.document.IntField;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.index.IndexWriterConfig.*;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
+
 
 import crawler.Link;
 import crawler.Tweet;
 import twitter4j.GeoLocation;
 import twitter4j.Place;
+
+import com.google.gson.Gson;
 
 public class Index {
 	
@@ -57,76 +43,92 @@ public class Index {
 				luceneConfig.setOpenMode(OpenMode.CREATE_OR_APPEND);
 			}
 			return (new IndexWriter (indexDir, luceneConfig));
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;	
 	}
 
-	protected void addDocuments(Tweet[] tweets, IndexWriter indexWriter) throws Exception {
-		for(Tweet tweet : tweets) {
-			Document doc = new Document();
-			
-			doc.add(new StringField("screenName", tweet.getScreenName(), Field.Store.YES));
-			doc.add(new StringField("name", tweet.getName(),Field.Store.YES));
-			doc.add(new StringField("timestamp",
-					DateTools.dateToString(tweet.getTimestamp(), DateTools.Resolution.MINUTE),
-					Field.Store.YES));
-			//Place
-			Place place = tweet.getPlace();
-			if(place != null) {
-				GeoLocation[][] geo = place.getBoundingBoxCoordinates();
-				doc.add(new DoubleField("latitude", geo[0][0].getLatitude(),Field.Store.YES));
-				doc.add(new DoubleField("longitude", geo[0][0].getLongitude(),Field.Store.YES));
-				doc.add(new StringField("placeName", place.getFullName(),Field.Store.YES));
-			}
-			doc.add(new TextField("text", "", Field.Store.YES));
-			
+	protected static void addDocuments(Tweet tweet, IndexWriter indexWriter) throws Exception {
+
+		Document doc = new Document();
+		
+		doc.add(new StringField("screenName", tweet.getScreenName(), Field.Store.YES));
+		doc.add(new StringField("name", tweet.getName(),Field.Store.YES));
+		doc.add(new StringField("timestamp",
+				DateTools.dateToString(tweet.getTimestamp(), DateTools.Resolution.MINUTE),
+				Field.Store.YES));
+
+		Place place = tweet.getPlace();
+		if(place != null) {
+			GeoLocation[][] geo = place.getBoundingBoxCoordinates();
+			doc.add(new DoubleField("latitude", geo[0][0].getLatitude(),Field.Store.YES));
+			doc.add(new DoubleField("longitude", geo[0][0].getLongitude(),Field.Store.YES));
+			doc.add(new StringField("placeName", place.getFullName(),Field.Store.YES));
+		}
+		doc.add(new TextField("text", "", Field.Store.YES));
+		
+		if(tweet.getLinks() != null) {
 			for(Link link : tweet.getLinks()) {
 				doc.add(new StringField("link", link.getTitle(), Field.Store.YES));
 			}
-			
+		}
+		
+		if(tweet.getHashtags() != null) {
 			for(String hashtag : tweet.getHashtags()) {
 				doc.add(new StringField("hashtag", hashtag, Field.Store.YES));
 			}
-			
-			doc.add(new TextField("text", tweet.getLang(), Field.Store.YES));
-			
-			indexWriter.addDocument(doc);
 		}
-		//indexWriter.commit();
-		//indexWriter.close();
+		
+		doc.add(new TextField("text", tweet.getLang(), Field.Store.YES));
+		
+		indexWriter.addDocument(doc);
 		
 	}
-	
-	public Tweet[] parseJSONFile(String filePath) {
-		InputStream jsonFile = getClass().getResourceAsStream(filePath);
-		Reader readerJSON = new InputStreamReader(jsonFile);
-		
-		Tweet tweets = (Tweet) JSONValue.parse(readerJSON);
-		//JSONArray arrayObjects = (JSONArray) fileObjects;
-		
-		return tweets;
-		
-	}
-	
-	public void createIndex(String filePath, String indexPath) {
-		
-		IndexWriter indexWriter = getIndexWriter(Paths.get(indexPath));
-		
-	}
-	
+
+	/*
 	public static void search(String indexDir, String query) throws IOException, ParseException{
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexDir)));
 		IndexSearcher indexSearcher = new IndexSearcher(reader);
 		
 		QueryParser parser = new QueryParser()
 	}
+	*/
+	
+	public static void parseJSONIntoIndex(IndexWriter indexWriter, String filePath) {
+		
+		Gson gson = new Gson();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filePath));
+			
+			String json = br.readLine();
+			Tweet tweet;
+			while(json != null) {
+				tweet = gson.fromJson(json, Tweet.class);
+			
+				addDocuments(tweet, indexWriter);
+				json = br.readLine();
+			}
+			br.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) {
 		
-		JSONArray jsonObjects = parseJSONFile(args[1]);
-		IndexWriter indexWriter = getIndexWriter(Paths.get(args[0]), Boolean.valueOf(args[2]));
+		IndexWriter indexWriter = getIndexWriter(Paths.get("/Index/i1"), true);
+		parseJSONIntoIndex(indexWriter,"Tweets/tweet1.json");
+		System.out.println("JSON Loaded into index");
 		
-
+		try {
+			indexWriter.commit();
+			indexWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
